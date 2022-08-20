dc26eac3789e607b6e5e21b15da540b8b4c1039c
Geary <gearyaudie@gmail.com>
2022-08-20 21:11:12 +0700

    fix build problems

--- components/ArticleComponent.tsx
+++ components/ArticleComponent.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const ArticleComponent = () => {
   return (
     <>
       <div style={{ marginTop: 150 }}>
-        <a href="/artikel/lem-vinyl-parket-pvc">Go to article</a>
+        <Link href="/artikel/lem-vinyl-parket-pvc">Go to article</Link>
       </div>
     </>
   );
